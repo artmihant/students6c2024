a3118bce26d12522a64cf66aa052ee6076e723ab
artem <Artem Antonov>
2024-10-14 23:05:29 +0300

    0.0.5

--- lesson2.py
+++ lesson2.py
@@ -1,23 +1,57 @@
-# Моделирование математического маятника методом Эйлера и Рунге-Кутты #
+# Моделирование двойного математического маятника методом Эйлера и Рунге-Кутты #
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 # PHYSICS #
 
-Gamma = 0
-Omega = 1
+L1 = 1
+L2 = 1
+M1 = 1
+M2 = 1
+
+G = 1
+
+def H(s):
+    q1 = s[:,0]
+    q2 = s[:,1]
+    p1 = s[:,2]
+    p2 = s[:,3]
+
+    f = L1*L2*(M1+M2*np.sin(q1-q2)**2)
+
+    return (M2*L2**2*p1**2 + (M1+M2)*L1**2*p2**2 - 2*M2*L1*L2*p1*p2*np.cos(q1-q2) )/(2*M2*L1*L2*f) - \
+       (M1+M2)*G*L1*np.cos(q1) - M2*G*L2*np.cos(q2)
+
+# Решаем уравнение s' = D(s) #
 
 def D(s):
-    u = s[0]
-    v = s[1]
-    return np.array(( v, -2*Gamma*v-Omega**2*np.sin(u)))
+    q1 = s[0]
+    q2 = s[1]
+    p1 = s[2]
+    p2 = s[3]
 
+    f = L1*L2*(M1+M2*np.sin(q1-q2)**2)
 
-U_0 = 0
-V_0 = 2
+    h1 = p1*p2*np.sin(q1-q2)/f
+    h2 = (M2*L2**2*p1**2 + (M1+M2)*L1**2*p2**2 - 2*M2*L1*L2*p1*p2*np.cos(q1-q2))/(2*f**2)
 
-S_0 = np.array((U_0, V_0), dtype=np.float32)
+    return np.array((
+        (L2*p1 - L1*np.cos(q1-q2)*p2 )/(L1*f),
+
+        (-M2*L2*np.cos(q1-q2)*p1 + (M1+M2)*L1*p2)/(M2*L2*f),
+
+        -(M1+M2)*G*L1*np.sin(q1) - h1 + h2*np.sin(2*(q1-q2)),
+
+        -M2*G*L2*np.sin(q2) + h1 - h2*np.sin(2*(q1-q2))
+    ))
+
+Q1_0 = 0
+Q2_0 = 1
+P1_0 = 0
+P2_0 = 0
+
+S_0 = np.array((Q1_0, Q2_0, P1_0, P2_0), dtype=np.float32)
 
 # NUMERIC #
 
@@ -29,34 +63,13 @@ StepsNumber = int( (T_max-T_min) / DeltaT )
 
 # Make state history array
 
-STrajectory = np.zeros((StepsNumber+1 , 2), dtype=np.float32)
+StateHistory = np.zeros((StepsNumber+1 , 4), dtype=np.float32)
 
-STrajectory[0] = S_0
+StateHistory[0] = S_0
 
 TimeAxes = np.linspace(T_min, T_max, StepsNumber+1)
 
 # METHOD #
-
-def explicit_euler_step(s):
-    d1 = DeltaT*D(s)
-    return s + d1
-
-def implicit_euler_step(s):
-    d1 = DeltaT*D(s)
-    d2 = DeltaT*D(s+d1)
-    return s + d2
-
-def average_exp_imp_step(s):
-    #RK2
-    d1 = DeltaT*D(s)
-    d2 = DeltaT*D(s+d1)
-    return s + (d1+d2)/2
-
-def half_point_step(s):
-    #RK2
-    d1 = DeltaT*D(s)
-    d2 = DeltaT*D(s+d1/2)
-    return s + d2
 
 def RK4_step(s):
     #RK4
@@ -71,68 +84,54 @@ def RK4_step(s):
 # Prosessing #
 
 ## Euler ##
-for i in range(1, StepsNumber+1 ):
-    STrajectory[i] = RK4_step(STrajectory[i-1])
+for i in range(StepsNumber):
+    StateHistory[i+1] = RK4_step(StateHistory[i])
 
 
-UTrajectory = STrajectory[:, 0]
-VTrajectory = STrajectory[:, 1]
+Enegry = H(StateHistory)
 
-Enegry = VTrajectory**2 + Omega**2 * 2*(1-np.cos(UTrajectory))
-
-
-
-# plt.plot(TimeAxes, Enegry)
+plt.plot(TimeAxes, Enegry)
 
 # plt.plot(TimeAxes, UTrajectory)
 # plt.plot(TimeAxes, VTrajectory)
 
+# plt.show()
 
-fig, axs  = plt.subplots(3, layout='constrained')
+def show_pendulum_move(StateHistory):
 
-axs[0].plot(TimeAxes, UTrajectory, label='U')
-axs[0].plot(TimeAxes, VTrajectory, label='V')
-axs[0].set(xlabel='T', ylabel='U,V', title='U(t) и V(t)')
+    Q1History = StateHistory[:,0]
+    Q2History = StateHistory[:,1]
 
-axs[1].plot(TimeAxes, Enegry)
-axs[1].set(xlabel='T', ylabel='E', title='E(t)')
+    Pendelium = np.zeros((2,3))
 
-axs[2].plot(UTrajectory, VTrajectory)
-axs[2].set(xlabel='U', ylabel='V', title='Фазовая траектория')
+    Fig, Ax = plt.subplots()
 
+    PendeliumPlot = Ax.plot(Pendelium[1], Pendelium[0],  marker='o')[0]
 
-def show_pendulum_move(UTrajectory):
+    Ax.set_xlim(-2.5, 2.5)
+    Ax.set_ylim(-2.5, 2.5)
 
-    L = 1
-
-    Trajectory = (-L*np.cos(UTrajectory), L*np.sin(UTrajectory))
-
-    fig, ax = plt.subplots()
-    # PhaseLine = ax.plot(Trajectory[0], Trajectory[1], label=f'Фазовая траектория маятника')[0]
-
-    LineTrajectory = ax.plot(Trajectory[1], Trajectory[0],  marker='o')[0]
-
-    plt.xlim(-1.2,1.2)
-    plt.ylim(-1.2,1.2)
-
-    ax.set(xlabel='X', ylabel='Y', )
-    ax.legend()
+    Ax.set(xlabel='X', ylabel='Y', )
+    Ax.legend()
 
     def loop_animation(i):
         """ Главный цикл вычисления/анимации """
 
-        LineTrajectory.set_data(Trajectory[1][i:i+5], Trajectory[0][i:i+5])
+        Pendelium[0][1] = -L1*np.cos(Q1History[i])
+        Pendelium[1][1] = L1*np.sin(Q1History[i])
+        Pendelium[0][2] = -L1*np.cos(Q1History[i]) - L2*np.cos(Q2History[i])    
+        Pendelium[1][2] = L1*np.sin(Q1History[i]) + L2*np.sin(Q2History[i])  
 
-        return (LineTrajectory)
+        PendeliumPlot.set_data(Pendelium[1], Pendelium[0])
+
+        return (PendeliumPlot)
 
     ani = animation.FuncAnimation(
-        fig=fig, 
+        fig=Fig, 
         func=loop_animation, 
         frames=StepsNumber, 
-        interval=0.02,
-        repeat=True,
-        repeat_delay=1000
+        interval=DeltaT*100
     )
     plt.show()
 
-show_pendulum_move(UTrajectory)
+show_pendulum_move(StateHistory)
